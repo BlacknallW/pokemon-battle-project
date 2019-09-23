d7a78e9733a17d3048c11dc76179a82678a7155c
William Blacknall <williamblacknall@gmail.com>
2019-09-23 10:10:02 -0400

    Added main function and test battle. It's somewhat impossible to win.

--- pokemon.py
+++ pokemon.py
@@ -6,33 +6,33 @@ class Pokemon:
         self.max_health = max_health
         self.current_health = current_health
         self.is_knocked_out = is_knocked_out
-        self.damage = self.attack
+        self.damage = self.level
 
     def attack(self,enemy):
         if self.ptype == "fire" and enemy.ptype == "water":
-            self.damage /= 2
+            slash = self.damage / 2
         if self.ptype == "fire" and enemy.ptype == "grass":
-            self.damage *= 2
+            slash = self.damage * 2
         if self.ptype == "water" and enemy.ptype == "grass":
-            self.damage /= 2
+            slash = self.damage / 2
         if self.ptype == "water" and enemy.ptype == "fire":
-            self.damage *= 2
+            slash = self.damage * 2
         if self.ptype == "grass" and enemy.ptype == "fire":
-            self.damage /= 2
+            slash = self.damage / 2
         if self.ptype == "grass" and enemy.ptype == "water":
-            self.damage *2
+            slash = self.damage * 2
 
-        enemy.current_health -= self.damage
-        print(f"{self.name} has dealt {self.damage} to {enemy.name}!\n\n")
+        enemy.current_health -= slash
+        print(f"{self.name} has dealt {slash} damage to {enemy.name}! {enemy.name} has {enemy.current_health} HP remaining!\n\n")
     
     def faint(self):
-        if self.current_health == 0:
+        if self.current_health < 0:
             self.is_knocked_out = True
             print(f"{self.name} has fainted and is unable to battle!\n\n")
     
     def recover(self):
         self.current_health += 30
-        print(f"{self.name} has recovered 30 health! {self.current_health} health remaining!")
+        print(f"{self.name} has recovered 30 HP! {self.current_health} HP remaining!\n\n")
 
 class Trainer:
     def __init__(self, name, pokemon, potions):
@@ -43,7 +43,7 @@ class Trainer:
 
     def use_potion(self,pokemon):
         pokemon.current_health += 50
-        print(f"{self.name} used a potion on {pokemon.name}! {pokemon.name} has recovered 50 health! {pokemon.current_health} remaining!")
+        print(f"{self.name} used a potion on {pokemon.name}! {pokemon.name} has recovered 50 HP! {pokemon.current_health} HP remaining!\n\n")
     
 
 charmander = Pokemon("Charmander", 10, "fire", 36, 36, False)
@@ -51,3 +51,33 @@ blastoise = Pokemon("Blastoise", 30, "water", 110, 110, False)
 
 sam = Trainer("Sam", [blastoise], 3)
 terry = Trainer("Terry", [charmander], 3)
+
+def main():
+    print("Welcome to the world of Pokemon! Here, 10 year olds are sent off to be killed by adult criminal syndicates and their murderbeasts. It's a great time for everyone!\n\nLook, there's one now! Eat his heart!\n\n")
+
+    def trainer_battle():
+        while Pokemon.faint != True:
+            print("Whatchu gon' do?")
+            print("1. Attack")
+            print("2. Use Potion")
+            user_input = input("")
+
+            if user_input == "1":
+                charmander.attack(blastoise)
+                if blastoise.current_health == 0:
+                    blastoise.faint()
+                    exit()
+            if user_input == "2":
+                terry.use_potion(charmander)
+            
+            while charmander.current_health > 0:
+                blastoise.attack(charmander)
+            else:
+                charmander.faint()
+                exit()
+
+
+    trainer_battle()
+
+    print("Great job! You beat that guy and stole his wallet. Aquired 152 whateverPokemon'scurrencyis!")
+main()
