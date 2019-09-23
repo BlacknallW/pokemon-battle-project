410d2ab6b08237ea7a2bc9a1c8e4e08f3c689f5f
William Blacknall <williamblacknall@gmail.com>
2019-09-23 09:39:33 -0400

    Created Trainer class with methods, created sample pokemon/trainers

--- pokemon.py
+++ pokemon.py
@@ -21,10 +21,33 @@ class Pokemon:
             self.damage /= 2
         if self.ptype == "grass" and enemy.ptype == "water":
             self.damage *2
-            
+
         enemy.current_health -= self.damage
         print(f"{self.name} has dealt {self.damage} to {enemy.name}!\n\n")
     
     def faint(self):
         if self.current_health == 0:
+            self.is_knocked_out = True
             print(f"{self.name} has fainted and is unable to battle!\n\n")
+    
+    def recover(self):
+        self.current_health += 30
+        print(f"{self.name} has recovered 30 health! {self.current_health} health remaining!")
+
+class Trainer:
+    def __init__(self, name, pokemon, potions):
+        self.name = name
+        self.pokemon = pokemon
+        self.potions = potions
+        self.active_pokemon = 1
+
+    def use_potion(self,pokemon):
+        pokemon.current_health += 50
+        print(f"{self.name} used a potion on {pokemon.name}! {pokemon.name} has recovered 50 health! {pokemon.current_health} remaining!")
+    
+
+charmander = Pokemon("Charmander", 10, "fire", 36, 36, False)
+blastoise = Pokemon("Blastoise", 30, "water", 110, 110, False)
+
+sam = Trainer("Sam", [blastoise], 3)
+terry = Trainer("Terry", [charmander], 3)
